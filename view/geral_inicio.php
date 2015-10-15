ef957cefe31c1a9e775d405e8ca6ccd7a58f4010
carlos <eeduardosantiago@gmail.com>
2015-10-15 08:28:06 -0300

    carlos 08:27 15/10/15

--- view/geral_inicio.php
+++ view/geral_inicio.php
@@ -3,7 +3,7 @@
 include_once '../controller/ControllerUsuario.php';
 
 include_once '_header.php';
-
+include_once '_menuGeral.php';
 try {
     $controllerUsuario = new ControllerUsuario();
    // $d = new UsuarioDao();
@@ -15,40 +15,6 @@ try {
 
 
 ?>
-        
-<nav id="navMain"class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-        <a  class="navbar-brand" href="#"><p>Home</p></a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-          <li ><a href="#"><p>Contato</p><span class="sr-only">(current)</span></a></li>
-
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><p>Informações<span class="caret"></span></p></a>
-          <ul class="dropdown-menu">
-              <li><a href="#"><p>Informações Técnicas</p></a></li>
-              <li><a href="#"><p>Sobre a Empresa</p></a></li>
-              <li><a href="#"><p>Informações adicionais</p></a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
-
 <div id="corpo" class="jumbotron">        
     <div class="container" >
     <div>
